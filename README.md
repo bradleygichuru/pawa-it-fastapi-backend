cbcc49fa0bfc69687a67a4c34edaf525792b9389
bradleygichuru <bradleygichuru@gmail.com>
2025-04-28 20:56:54 +0300

    DOC:document code

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ use pip to install required packages
 ```bash
 pip install -r requirements.txt
 ```
-set Gemini API key in environment variable template i.e .env
+set Gemini API key in environment variable template i.e .env obtained at https://aistudio.google.com/app/apikey?pli=1
 ```bash
 GEMINI_API_KEY="your api key"
 ```
--- server.py
+++ server.py
@@ -4,18 +4,18 @@ from typing import Annotated
 from fastapi import FastAPI, Form
 from google import genai
 from fastapi.middleware.cors import CORSMiddleware
-load_dotenv()
+load_dotenv()#load environment variables from .env file 
 
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))#Initiallize genai client using your gemini api key
 
-app = FastAPI()
+app = FastAPI()#intialize FastApi class
 
 origins = [
     "http://localhost.tiangolo.com",
     "https://localhost.tiangolo.com",
     "http://localhost:3000",
     "http://localhost:8080",
-]
+] #Allow Cross origin requests to aid in Dev testing 
 
 app.add_middleware(
     CORSMiddleware,
@@ -26,7 +26,7 @@ app.add_middleware(
 )
 @app.post("/ask")
 async def ask(query: Annotated[str, Form()]):
-    response = client.models.generate_content(model="gemini-2.0-flash", contents=query)
+    response = client.models.generate_content(model="gemini-2.0-flash", contents=query)#Query Gemini using query obtained from form data
     print(response)
     print(response.text)
     return response.text
